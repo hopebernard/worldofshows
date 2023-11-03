c6025227491dff5559aa107ccedefc2c84c64ddf
hope.bernard <hope.bernard.96@gmail.com>
2023-11-03 17:46:27 +0100

    optimisation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,49 @@
 "use strict"
 
+
+
+const lazyImages = document.querySelectorAll('img[data-src],source[data-srcset]');
+const windowHeight = document.documentElement.clientHeight;
+
+let lazyImagesPositions = [];
+if (lazyImages.length > 0) {
+    lazyImages.forEach(img => {
+        if (img.dataset.src || img.dataset.srcset) {
+            lazyImagesPositions.push(img.getBoundingClientRect().top + pageYOffset);
+            lazyScrollCheck();
+        }
+    });
+}
+
+window.addEventListener('scroll', lazyScroll);
+
+function lazyScroll() {
+    if (document.querySelectorAll('img[data-src],source[data-srcset]').length > 0) {
+        lazyScrollCheck();
+    }
+}
+
+function lazyScrollCheck() {
+    let imgIndex = lazyImagesPositions.findIndex(
+        item => pageYOffset > item - windowHeight
+    );
+    if (imgIndex >= 0) {
+        if (lazyImages[imgIndex].dataset.src) {
+            lazyImages[imgIndex].src = lazyImages[imgIndex].dataset.src;
+            lazyImages[imgIndex].removeAttribute('data-src');
+        } else if (lazyImages[imgIndex].dataset.srcset) {
+            lazyImages[imgIndex].srcset = lazyImages[imgIndex].dataset.srcset;
+            lazyImages[imgIndex].removeAttribute('data-srcset');
+        }
+        delete lazyImagesPositions[imgIndex];
+    }
+}
+
+
+
+
+
+
 const burgerIcon = document.querySelector('.icon-menu');
 const burgerMenu = document.querySelector('.menu');
 
